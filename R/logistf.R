f6cfb14cf2d3beb6fa35bb141926a412f15acedc
yifanfan1998 <fanyifan1998@163.com>
2026-04-27 19:52:44 +0800

    Update logistf.R

--- R/logistf.R
+++ R/logistf.R
@@ -532,7 +532,8 @@ function(formula, data, pl = TRUE, alpha = 0.05, control, plcontrol, modcontrol,
             fit.i<-logistf.fit(x,y, weight=weight, offset=offset, firth, control=control, modcontrol = modcontrolpl)
             pl.iter[i,3]<-fit.i$iter
             #fit$prob[i] <- 1-pchisq(2*(fit.full$loglik-fit.i$loglik),1)
-            fit$prob[i] <- pchisq(2*(fit.full$loglik-fit.i$loglik), 1, lower.tail = FALSE , log.p = TRUE)
+            log_p <- pchisq(2*(fit.full$loglik-fit.i$loglik), 1, lower.tail = FALSE , log.p = TRUE)
+            fit$prob[i] <- mpfr(log_p, precBits = 128)
             fit$method.ci[i] <- "Profile Likelihood"
         }
           
